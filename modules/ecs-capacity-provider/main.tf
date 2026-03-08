5f0022c13e1157e3339add2a44b2052093ed179e
Tecnologia Gsoft <tecnologia@gsoftcolombia.com>
2026-03-08 17:16:51 +0100

    it is not really required and more cost-optimized

--- modules/ecs-capacity-provider/main.tf
+++ modules/ecs-capacity-provider/main.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_capacity_provider" "this" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = module.autoscaling.autoscaling_group_arn
-    managed_termination_protection = "ENABLED"
+    managed_termination_protection = "DISABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1
