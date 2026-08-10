f6e1182c490b07d9ba210a52b565c48b51285c80
Tecnologia Gsoft <tecnologia@gsoftcolombia.com>
2026-08-10 08:17:22 +0200

    Refactor IAM policy handling to use inline statements instead of ARNs for better flexibility

--- modules/ecs-scheduled-task/iam-task.tf
+++ modules/ecs-scheduled-task/iam-task.tf
@@ -24,25 +24,21 @@ resource "aws_iam_role_policy_attachment" "task_custom_policy" {
   policy_arn = aws_iam_policy.task.arn
   role       = aws_iam_role.task.name
 }
-resource "aws_iam_role_policy_attachment" "additional_task_policy" {
-  count      = length(var.container_additional_iam_policy_arns)
-  policy_arn = var.container_additional_iam_policy_arns[count.index]
-  role       = aws_iam_role.task.name
-}
 
 resource "aws_iam_policy" "task" {
   name = "${var.name_prefix}-${var.environment}-${var.execution_name}-task"
   path = "/ecs/"
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ssm:GetParameters"
-        ]
-        Resource = "*"
-      }
-    ]
+    Statement = concat(
+      [
+        {
+          Effect   = "Allow",
+          Action   = ["ssm:GetParameters"]
+          Resource = "*"
+        }
+      ],
+      var.additional_policy_statements
+    )
   })
 }
--- modules/ecs-scheduled-task/variables.tf
+++ modules/ecs-scheduled-task/variables.tf
@@ -75,8 +75,8 @@ variable "security_groups" {
   description = "List of Security Groups where the task will be running."
   type        = list(string)
 }
-variable "container_additional_iam_policy_arns" {
-  description = "ARN of an additional IAM Policy to grant permissions to the task (e.g. sts:AssumeRole on cross-account roles)"
-  type        = list(string)
+variable "additional_policy_statements" {
+  description = "Inline IAM policy statements merged into the task policy. Use this instead of container_additional_iam_policy_arns when the policy is new and its ARN would be unknown at plan time."
+  type        = list(any)
   default     = []
 }
